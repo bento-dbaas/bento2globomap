6bf9ebcf6d9cb3802ec0794a6e10165678214f67
otherpirate <mauro_murari@hotmail.com>
2018-01-18 16:53:14 -0200

    Add remove_before_ts parameter in update

--- map.py
+++ map.py
@@ -2,36 +2,24 @@ from requests import post
 from bento_map.settings import MAP_ENDPOINT
 
 
-def update(models):
+def update(models, remove_before_at):
     content = list()
     collections = {}
-    lower_timestamp = None
     for model in models:
         content.append(model.content)
         collections[model.collection] = model.type
 
-        if not lower_timestamp:
-            lower_timestamp = model.created_at_ts
-        if lower_timestamp > model.created_at_ts:
-            lower_timestamp = model.created_at_ts
-
-    for collection, type in collections.items():
+    for collection, collection_type in collections.items():
         content.append({
             "action": "CLEAR",
             "collection": collection,
-            "element": [
-                [
-                    {
-                    "field": "timestamp",
-                        "operator": "<",
-                        "value": lower_timestamp
-                    }
-                ]
-            ],
-            "type": type
+            "element": [[{
+                "field": "timestamp",
+                "operator": "<",
+                "value": remove_before_at - 1
+            }]],
+            "type": collection_type
         })
 
     url = MAP_ENDPOINT + "/v1/updates"
-    print(content)
-
     return post(url, json=content)
